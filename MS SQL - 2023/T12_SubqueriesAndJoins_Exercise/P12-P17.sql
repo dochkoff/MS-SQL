451678b2074b48c14c0e0810c782ed2b7c7bd71e
dochkoff <hounds.07cinder@icloud.com>
2023-10-05 19:00:35 +0300

    T12-P16,P17

--- MS SQL - 2023/T12_SubqueriesAndJoins_Exercise/P12-P17.sql
+++ MS SQL - 2023/T12_SubqueriesAndJoins_Exercise/P12-P17.sql
@@ -43,3 +43,32 @@ FROM
     Where CurrencyUsage > 1) AS SocondSubQuery
 WHERE CurremcyRank = 1
 ORDER BY ContinentCode
+
+--P16
+SELECT COUNT(*) AS Count
+FROM
+    (SELECT mc.CountryCode AS ForCount,c.CountryCode
+    FROM MountainsCountries AS mc
+    JOIN Mountains AS m ON m.Id = mc.MountainId
+    RIGHT JOIN Countries AS c ON c.CountryCode = mc.CountryCode) AS CoreQuery
+WHERE ForCount IS NULL
+
+--P17
+SELECT TOP(5)
+    c.CountryName,
+    MAX(p.Elevation) AS HighestPeakElevation,
+    MAX(r.Length) AS LongestRiverLength
+FROM
+    Countries c
+LEFT JOIN MountainsCountries mc ON c.CountryCode = mc.CountryCode
+LEFT JOIN Mountains m ON mc.MountainId = m.Id
+LEFT JOIN Peaks p ON m.Id = p.MountainId
+LEFT JOIN CountriesRivers cr ON c.CountryCode = cr.CountryCode
+LEFT JOIN Rivers r ON cr.RiverId = r.Id
+GROUP BY c.CountryName
+ORDER BY
+    HighestPeakElevation DESC,
+    LongestRiverLength DESC,
+    c.CountryName ASC;
+
+
